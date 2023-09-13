b607c07e6f44bf5bc73c2a9252ca79959439eb6d
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2023-09-13 16:26:36 +0200

    change

--- Project_4-quiz/Program.cs
+++ Project_4-quiz/Program.cs
@@ -1,98 +1,60 @@
 ﻿using System;
-string choice = " ";
 
-int sum = 0;
+int points = 0;
+Console.WriteLine("Välkommen till mitt sifferquiz!\nSvara på frågorna genom att skriva rätt siffra för rätt svarsalternativ och tryck på ENTER.");
 
-Console.Title = "quiz";
-Console.ForegroundColor = ConsoleColor.Magenta;
-Console.BackgroundColor = ConsoleColor.Gray;
-
-Console.WriteLine("Välkommen till mitt sifferquiz!");
-Console.WriteLine("Svara på frågorna genom att skriva rätt siffra för rätt svarsalternativ och tryck på ENTER.");
-Console.WriteLine("Tryck på ENTER när du är redo för första frågan.");
-
-choice = Console.ReadLine();
-choice = choice.ToLower();
-
-Console.WriteLine("Första frågan:");
-Console.WriteLine("Vilket alternativ tror du är rätt?");
-Console.WriteLine("1. 3");
-Console.WriteLine("2. 1");
-Console.WriteLine("3. 2");
-
-choice = Console.ReadLine();
-choice = choice.ToLower();
-
-if (choice == "1" | choice == "2" | choice == "3")
+Console.WriteLine("Fråga 1: \nVilket alternativ tror du är rätt?");
+Console.WriteLine("1. 3  \n2. 1  \n3. 2");
+string answer1 = Console.ReadLine();
+if (answer1 == "1")
 {
-    if (choice == "1")
-    {sum++;
-    }
-    Console.WriteLine("Andra frågan:");
-    Console.WriteLine("Vilket alternativ tror du är rätt nu?");
-    Console.WriteLine("1. 2");
-    Console.WriteLine("2. 3");
-    Console.WriteLine("3. 1");
-
-    choice = Console.ReadLine();
-    choice = choice.ToLower();
-    
-    if (choice == "1" | choice == "2" | choice == "3")
-    {
-        if (choice == "2")
-        {sum++;
-        }
-        Console.WriteLine("Tredje och sista frågan:");
-        Console.WriteLine("Vilket alternativ tror du är rätt nu?");
-        Console.WriteLine("1. 3");
-        Console.WriteLine("2. 1");
-        Console.WriteLine("3. 2");
-
-        choice = Console.ReadLine();
-        choice = choice.ToLower();
-
-        if (choice == "1" | choice == "2" | choice == "3")
-        {
-            if (choice == "1")
-            {sum++;
-            }
-            
-            if (sum == 3)
-            {
-                Console.WriteLine("Du fick alla rätt. Grymt jobbat!");
-            }
-            else if (sum == 2)
-            {
-                Console.WriteLine("Nästan alla rätt! Du fick ett fel.");
-            }
-            else if (sum == 1)
-            {
-                Console.WriteLine("Ett är bättre än inget. Du fick en poäng.");
-            }
-            else if (sum == 0)
-            {
-                Console.WriteLine("Wow ... var quizet för svårt eller är det du som inte kan något? Du fick noll poäng.");
-            }
-            else
-            {
-                Console.WriteLine("Du skrev något som jag inte förstod. Hejdå.");
-                choice = Console.ReadLine();
-                choice = choice.ToLower();
-            }
-            choice = Console.ReadLine();
-            choice = choice.ToLower();
-        }
-    }
-    else
-    {
-        Console.WriteLine("Du skrev något som jag inte förstod. Hejdå.");
-        choice = Console.ReadLine();
-        choice = choice.ToLower();
-    }
+  points++;
+  Console.WriteLine("  Du gissade rätt!");
 }
 else
 {
-    Console.WriteLine("Du skrev något som jag inte förstod. Hejdå.");
-    choice = Console.ReadLine();
-    choice = choice.ToLower();
+  Console.WriteLine("  Fel svar...");
 }
+
+Console.WriteLine("Fråga 2: \nVilket alternativ tror du är rätt nu?");
+Console.WriteLine("1. 2 \n2. 3 \n3. 1");
+string answer2 = Console.ReadLine();
+if (answer2 == "2")
+{
+  points++;
+  Console.WriteLine("  Du gissade rätt!");
+}
+else
+{
+  Console.WriteLine("  Fel svar...");
+}
+
+Console.WriteLine("Fråga 3: \nVilket alternativ tror du är rätt nu?");
+Console.WriteLine("1. 3 \n2. 1 \n3. 2");
+string answer3 = Console.ReadLine();
+if (answer3 == "1")
+{
+  points++;
+  Console.WriteLine("  Du gissade rätt!");
+}
+else
+{
+  Console.WriteLine("  Fel svar...");
+}
+
+Console.WriteLine($"Du fick {points} poäng.");
+if (points == 0)
+{
+  Console.WriteLine("Var quizet för svårt eller är det du som inte har någon koll?");
+}
+else if (points < 3)
+{
+  Console.WriteLine("Bra försökt! Du gissade rätt en eller ett par gånger.");
+}
+else 
+{
+  Console.WriteLine("Grattis! Du hade alla rätt!");
+}
+
+Console.WriteLine("Tryck på ENTER för att avsluta.");
+Console.ReadKey();
