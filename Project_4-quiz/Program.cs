affeb6247642e97b80f6da15502779e818b5413b
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2023-09-13 10:51:11 +0200

    change

--- Project_4-quiz/Program.cs
+++ Project_4-quiz/Program.cs
@@ -11,10 +11,10 @@ choice = Console.ReadLine();
 choice = choice.ToLower();
 
 Console.WriteLine("Första frågan:");
-Console.WriteLine("Vad");
-Console.WriteLine("1. ");
-Console.WriteLine("2. ");
-Console.WriteLine("3. ");
+Console.WriteLine("Vilket alternativ tror du är rätt?");
+Console.WriteLine("1. 3");
+Console.WriteLine("2. 1");
+Console.WriteLine("3. 2");
 
 choice = Console.ReadLine();
 choice = choice.ToLower();
@@ -25,10 +25,10 @@ if (choice == "1" | choice == "2" | choice == "3")
     {sum++;
     }
     Console.WriteLine("Andra frågan:");
-    Console.WriteLine("Vad");
-    Console.WriteLine("1. ");
-    Console.WriteLine("2. ");
-    Console.WriteLine("3. ");
+    Console.WriteLine("Vilket alternativ tror du är rätt nu?");
+    Console.WriteLine("1. 2");
+    Console.WriteLine("2. 3");
+    Console.WriteLine("3. 1");
 
     choice = Console.ReadLine();
     choice = choice.ToLower();
@@ -39,10 +39,10 @@ if (choice == "1" | choice == "2" | choice == "3")
         {sum++;
         }
         Console.WriteLine("Tredje och sista frågan:");
-        Console.WriteLine("Vad");
-        Console.WriteLine("1. ");
-        Console.WriteLine("2. ");
-        Console.WriteLine("3. ");
+        Console.WriteLine("Vilket alternativ tror du är rätt nu?");
+        Console.WriteLine("1. 3");
+        Console.WriteLine("2. 1");
+        Console.WriteLine("3. 2");
 
         choice = Console.ReadLine();
         choice = choice.ToLower();
@@ -53,24 +53,42 @@ if (choice == "1" | choice == "2" | choice == "3")
             {sum++;
             }
             
-            if (sum <= 3)
+            if (sum == 3)
             {
-                Console.WriteLine("Du fick alla rätt. Bra jobbat!");
+                Console.WriteLine("Du fick alla rätt. Grymt jobbat!");
             }
             else if (sum == 2)
             {
-                Console.WriteLine("Nästan! Du hade ett fel.");
+                Console.WriteLine("Nästan alla rätt! Du fick ett fel.");
             }
             else if (sum == 1)
             {
                 Console.WriteLine("Ett är bättre än inget. Du fick en poäng.");
             }
+            else if (sum == 0)
+            {
+                Console.WriteLine("Wow ... var quizet för svårt eller är det du som inte kan något? Du fick noll poäng.");
+            }
             else
             {
-                Console.WriteLine("Wow! Va quizet för svårt för dig? Du fick noll poäng.");
+                Console.WriteLine("Du skrev något som jag inte förstod. Hejdå.");
+                choice = Console.ReadLine();
+                choice = choice.ToLower();
             }
             choice = Console.ReadLine();
             choice = choice.ToLower();
         }
     }
+    else
+    {
+        Console.WriteLine("Du skrev något som jag inte förstod. Hejdå.");
+        choice = Console.ReadLine();
+        choice = choice.ToLower();
+    }
+}
+else
+{
+    Console.WriteLine("Du skrev något som jag inte förstod. Hejdå.");
+    choice = Console.ReadLine();
+    choice = choice.ToLower();
 }
